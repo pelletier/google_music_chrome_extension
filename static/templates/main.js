fc2e078a7b3df2e0b642bd2f935722b15abb017a
Thomas Pelletier <pelletier.thomas@gmail.com>
2013-12-20 11:43:40 +0100

    Fix bug bad characters display
    HTML entities were escaped when rendering in the mini player

--- static/templates/main.js
+++ static/templates/main.js
@@ -24,12 +24,12 @@ function program3(depth0,data) {
     + "\"/>\n  <div class='info'>\n    <h3>";
   if (stack1 = helpers.track) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.track); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</h3>\n    <h4>";
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "</h3>\n    <h4>";
   if (stack1 = helpers.author) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.author); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</h4>\n  </div>\n  <div class='controls'>\n    <a class='back' href=\"#\"><i class='fa fa-fast-backward fa-2x'/></a>\n    <a class='play' href=\"#\"><i class='fa fa-";
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "</h4>\n  </div>\n  <div class='controls'>\n    <a class='back' href=\"#\"><i class='fa fa-fast-backward fa-2x'/></a>\n    <a class='play' href=\"#\"><i class='fa fa-";
   stack1 = helpers['if'].call(depth0, (depth0 && depth0.isPlaying), {hash:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += " fa-2x'/></a>\n    <a class='next' href=\"#\"><i class='fa fa-fast-forward fa-2x'/></a>\n  </div>\n  <a class='open' href=\"#\"><i class='fa fa-external-link'/></a>\n</div>\n";
